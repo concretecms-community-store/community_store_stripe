dea15e4e05e75a62580155ddd4a630ac63e937b5
Ryan Hewitt <ryan@mesuva.com.au>
2016-02-23 21:58:22 +1030

    Better handling of email loading in Stripe form

--- elements/stripe/stripe_button.php
+++ elements/stripe/stripe_button.php
@@ -6,7 +6,6 @@
         var handler = StripeCheckout.configure({
             key: '<?= $publicAPIKey; ?>',
             locale: 'auto',
-            email: '<?= $email; ?>',
             token: function (token) {
                 // Use the token to create the charge with a server-side script.
                 // You can access the token ID with `token.id`
@@ -16,23 +15,26 @@
         });
 
         $('.store-btn-complete-order').on('click', function (e) {
+
+
             // Open Checkout with further options
             var currentpmid = $('input[name="payment-method"]:checked:first').data('payment-method-id');
 
             if (currentpmid == <?= $pmID; ?>) {
                 handler.open({
                     currency: "<?= strtolower($currency);?>",
-                    amount: <?= $amount; ?>
+                    amount: <?= $amount; ?>,
+                    email: <?=  ($email ? "'" . $email . "'" : '$(\'#store-email\').val()'); ?>
                 });
                 e.preventDefault();
-            } else {
-
             }
+
+            // Close Checkout on page navigation
+            $(window).on('popstate', function () {
+                handler.close();
+            });
         });
 
-        // Close Checkout on page navigation
-        $(window).on('popstate', function () {
-            handler.close();
-        });
+
     });
 </script>
