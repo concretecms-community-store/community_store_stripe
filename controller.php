779461feb3100cda362885cb68f26b02c893e231
Ryan Hewitt <ryan@mesuva.com.au>
2016-12-13 15:31:07 +1030

    Update package version

--- controller.php
+++ controller.php
@@ -14,7 +14,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'community_store_stripe';
     protected $appVersionRequired = '5.7.2';
-    protected $pkgVersion = '0.9.3';
+    protected $pkgVersion = '1.0';
     protected $pkgAutoloaderRegistries = array(
         'src/Omnipay/Stripe' => '\Omnipay\Stripe'
     );
