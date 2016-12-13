1c3ea367d776d19aa2b597f2ac50f9099c945ddc
Ryan Hewitt <ryan@mesuva.com.au>
2016-12-13 16:41:40 +1030

    Removal of unnecessary code, supports v8

--- controller.php
+++ controller.php
@@ -14,10 +14,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'community_store_stripe';
     protected $appVersionRequired = '5.7.2';
-    protected $pkgVersion = '1.0';
-    protected $pkgAutoloaderRegistries = array(
-        'src/Omnipay/Stripe' => '\Omnipay\Stripe'
-    );
+    protected $pkgVersion = '1.0.1';
 
     public function getPackageDescription()
     {
