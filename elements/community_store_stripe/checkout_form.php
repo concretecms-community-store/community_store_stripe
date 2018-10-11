cae09b1b10be0bccbd6208535323514f35c175d6
Jeff Paetkau <jeff@abcweblink.ca>
2018-10-11 15:05:14 -0700

    Add support for stripe.js v3 elements

--- elements/community_store_stripe/checkout_form.php
+++ elements/community_store_stripe/checkout_form.php
@@ -5,9 +5,11 @@ extract($vars);
 <?php if ($gateway == 'stripe_button') {
     Loader::packageElement('stripe/stripe_button', 'community_store_stripe', $vars);
 } ?>
-
 <?php if ($gateway == 'stripe_form') {
     Loader::packageElement('stripe/stripe_form', 'community_store_stripe', $vars);
+} ?>
+<?php if ($gateway == 'stripe_form_elements') {
+    Loader::packageElement('stripe/stripe_form_elements', 'community_store_stripe', $vars);
 } ?>
 <?php } else { ?>
 <p class="alert alert-warning"><?php t('API Keys not entered');?></p>
